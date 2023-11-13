6163a1f38dbf6f96266be9da89e4e4a76f674cf5
Rajesh1612169 <67611897+Rajesh1612169@users.noreply.github.com>
2023-11-14 02:37:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const express = require('express');
+const format = require('date-format');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World');
+});
+
+app.get('/api/v1/instagram', (req, res) => {
+    const instaSocial = {
+        username: 'Rajesh Kumar',
+        followers: 66,
+        follows: 50,
+        date: format.asString("dd-MM-yyyy - hh:mm:ss", new Date())
+    };
+
+    res.status(200).json(instaSocial);
+});
+
+app.get('/api/v1/:token', (req, res) => {
+    console.log(req.params.token);
+    res.status(200).json({param: req.params.token});
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
 })
-app.listen(process.env.PORT || 3000)
